aa369783f831b5e406c97da975bf562770134037
SamanAbbasi <samanabbasi.plus@gmail.com>
2023-12-07 22:40:36 +0330

    create cra for exercise 7

---
+++ samanabbasi-1402916-7/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import OrderShell from './components/OrderShell/OrderShell';
+
+function App() {
+  return (
+    <div className="App">
+      <OrderShell />
+    </div>
+  );
+}
+
+export default App;
